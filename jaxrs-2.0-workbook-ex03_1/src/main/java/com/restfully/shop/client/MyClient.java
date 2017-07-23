767446b1ae2d7321a01ac62ec5d930f0995d3274
Piotrek <Piotrek@192.168.1.171>
2017-07-23 10:57:58 +0200

    add client

---
+++ jaxrs-2.0-workbook-ex03_1/src/main/java/com/restfully/shop/client/MyClient.java
@@ -0,0 +1,35 @@
+package com.restfully.shop.client;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+public class MyClient {
+
+	public static void main(String[] args) {
+		Client client = ClientBuilder.newClient();
+
+		try {
+			System.out.println("*** Create a new Customer ***");
+			String xml = "<customer>" + "<first-name>kkkkk</first-name>" + "<last-name>dddd</last-name>"
+					+ "<street>256 Clarendon Street</street>" + "<city>aaa</city>" + "<state>MA</state>"
+					+ "<zip>02115</zip>" + "<country>USA</country>" + "</customer>";
+
+			Response response = client.target("http://localhost:8080/services/customers")
+					.request(MediaType.APPLICATION_XML)
+					.post(Entity.xml(xml));
+			if (response.getStatus() != 201) {
+				throw new RuntimeException("Failed to create");
+			}
+			
+			String location = response.getLocation().toString();
+			System.out.println("Location: " + location);
+			response.close();
+
+		} finally {
+			client.close();
+		}
+	}
+}
