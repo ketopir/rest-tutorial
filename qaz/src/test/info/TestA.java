cd861bb2c2ee9e36d6573c18d1a5b5519bbe63b9
Comarch <Comarch@10.132.6.114>
2017-11-08 13:00:29 +0100

    test1

---
+++ qaz/src/test/info/TestA.java
@@ -0,0 +1,15 @@
+/* @(#) $Id$
+ *
+ * Copyright (c) 2000-2017 ComArch S.A. All Rights Reserved. Any usage, duplication or redistribution of this
+ * software is allowed only according to separate agreement prepared in written between ComArch and authorized party.
+ */
+package test.info;
+
+/**
+ * @author Comarch
+ */
+public class TestA {
+    public static void main(String[] args) {
+        
+    }
+}
